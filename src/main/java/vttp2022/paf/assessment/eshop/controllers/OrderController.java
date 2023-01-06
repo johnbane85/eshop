a385c7e7dc782921732df9775ad5261ffbe6f9bf
johnbane85 <junyu.siew@gmail.com>
2023-01-06 16:57:30 +0800

    3rd update

--- src/main/java/vttp2022/paf/assessment/eshop/controllers/OrderController.java
+++ src/main/java/vttp2022/paf/assessment/eshop/controllers/OrderController.java
@@ -3,22 +3,27 @@ package vttp2022.paf.assessment.eshop.controllers;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.json.Json;
+import jakarta.json.JsonArrayBuilder;
 import jakarta.servlet.http.HttpSession;
 import vttp2022.paf.assessment.eshop.models.LineItem;
 import vttp2022.paf.assessment.eshop.models.Order;
 import vttp2022.paf.assessment.eshop.respositories.OrderException;
 import vttp2022.paf.assessment.eshop.respositories.OrderRepository;
 
-@Controller
-@RequestMapping(path = "/cart")
+@RestController
+@RequestMapping(path = "/api/eshop", produces = MediaType.APPLICATION_JSON_VALUE)
 public class OrderController {
 
 	// TODO: Task 3
@@ -26,7 +31,7 @@ public class OrderController {
 	private OrderRepository ordRepo;
 
 	@PostMapping
-	public String postOrder(@RequestBody MultiValueMap<String, String> form,
+	public ResponseEntity<String> postOrder(@RequestBody MultiValueMap<String, String> form,
 			Model model, HttpSession sess) {
 
 		List<LineItem> lineItems = (List<LineItem>) sess.getAttribute("cart");
@@ -53,7 +58,11 @@ public class OrderController {
 			e.printStackTrace();
 		}
 
-		return "checkout";
+		JsonArrayBuilder arrBuilder = Json.createArrayBuilder();
+		for (LineItem l : lineItems)
+			arrBuilder.add(l.toJson());
+
+		return ResponseEntity.ok(arrBuilder.build().toString());
 	}
 
 }
--- src/main/java/vttp2022/paf/assessment/eshop/models/LineItem.java
+++ src/main/java/vttp2022/paf/assessment/eshop/models/LineItem.java
@@ -1,5 +1,8 @@
 package vttp2022.paf.assessment.eshop.models;
 
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+
 // DO NOT CHANGE THIS CLASS
 public class LineItem {
 
@@ -26,4 +29,12 @@ public class LineItem {
 	public void setQuantity(Integer quantity) {
 		this.quantity = quantity;
 	}
+
+	public JsonObject toJson() {
+		return Json.createObjectBuilder()
+				.add("item", item)
+				.add("quantity", quantity)
+				.build();
+	}
+
 }
